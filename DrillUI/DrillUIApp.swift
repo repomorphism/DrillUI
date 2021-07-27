bf4b555679875959ca1d35b1a29d373cccf332fe
Hsing Liu <liu.hsing@gmail.com>
2021-07-28 01:38:14 +0800

    Initial Commit

---
+++ DrillUI/DrillUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillUIApp.swift
+//  DrillUI
+//
+//  Created by Paul on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DrillUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
