bacbd6f59ac963e86910348045a6d67dd2e9b9b3
Hsing Liu <liu.hsing@gmail.com>
2021-07-28 17:44:56 +0800

    Add iOS target

---
+++ DrillUI_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DrillUI_iOS
+//
+//  Created by Paul on 7/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DrillUI_iOS/DrillUI_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrillUI_iOSApp.swift
+//  DrillUI_iOS
+//
+//  Created by Paul on 7/28/21.
+//
+
+import SwiftUI
+
+@main
+struct DrillUI_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
